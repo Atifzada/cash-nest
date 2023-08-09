c43bbb6ce81146992df05174f23c9f62c3dfc905
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-09 13:00:41 +0500

    Style user registrations

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'pg'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.6'
 
+gem 'devise', '~> 4.9', '>= 4.9.2'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users 
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+  root 'groups#index'
+
+  resources :users do
+    resources :groups do
+      resources :cash
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'groups/index'
-  get 'groups/show'
-  root 'groups#index'
 end
