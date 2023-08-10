f7228c205d7804492ca5b2c54c7c3c650eca0c3a
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-10 20:14:26 +0500

    Add test for cashes

---
+++ spec/integrations/cashes_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Wallet', type: :feature do
+    describe 'Create new group' do
+        before(:each) do
+            User.destroy_all
+            Group.destroy_all
+            @user = User.create(name:"Atif", email:"atif@email.com", password:"asd123")
+            @group = Group.create(name:'easypaisa', icon: Rack::Test::UploadedFile.new('spec/media/easypaisa.png') )
+            visit new_user_session_path
+            fill_in 'Email', with: 'atif@email.com'
+            fill_in 'Password', with: 'asd123'
+            click_button 'Log in'
+            click_link 'Add New Category'
+            fill_in 'Name', with: 'easypaisa'
+            attach_file('Icon', Rails.root + 'spec/media/easypaisa.png')
+            click_button 'Submit'
+            visit root_path
+            click_link 'easypaisa'
+            click_link 'Add New'
+            fill_in 'Name', with: 'New_Item'
+            fill_in 'Amount', with: 200
+            click_button 'Submit'
+            expect(page).to have_content('easypaisa')
+        end
+        it 'Create new cash' do
+            visit root_path
+            click_link 'easypaisa'
+            click_link 'New_Item'
+            expect(page).to have_content('200')
+            expect(page).to have_content('easypaisa')
+            expect(page).to have_content('New_Item')
+            expect(page).to have_content('Go back')
+        end
+    end
+end
